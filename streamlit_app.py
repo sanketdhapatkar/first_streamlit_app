ffe0f6905a7d881a78f05aa162dd0c65e6637496
sanketdhapatkar <123308267+sanketdhapatkar@users.noreply.github.com>
2023-01-24 23:35:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,60 +30,67 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
+    
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("the fruit load list contains:")
+streamlit.dataframe(my_data_rows)
   
  #New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try : 
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-       back_from_function = get_fruityvice_data(fruit_choice)
-       streamlit.dataframe(back_from_function)
+#streamlit.header("Fruityvice Fruit Advice!")
+#try : 
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error("Please select a fruit to get information.")
+#  else:
+#       back_from_function = get_fruityvice_data(fruit_choice)
+#       streamlit.dataframe(back_from_function)
 
-except URLError as e:
-     streamlit.error()
+#except URLError as e:
+#     streamlit.error()
+#    
+#streamlit.write('The user entered ', fruit_choice)
+#
+-#import requests
+
+
+
+-#take the json version of response and normalize it
+
+-# write your own comment - what does this do?
+
+
+-#import snowflake.connector
+
+#streamlit.header("View Our Fruit List - Add Your Favorites!")
+-#Snowflake-related functions
+#def get_fruit_load_list():
+#    with my_cnx.cursor() as my_cur:
+#         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#         return my_cur.fetchall()
+#    
     
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-
-
-
-#take the json version of response and normalize it
-
-# write your own comment - what does this do?
+-# Add a button to load the fruit
+#if streamlit.button('Get Fruit List'):
+#    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#    my_data_rows = get_fruit_load_list()
+#    my_cnx.close()
+#    streamlit.dataframe(my_data_rows)
 
 
-#import snowflake.connector
-
-streamlit.header("View Our Fruit List - Add Your Favorites!")
-#Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-         return my_cur.fetchall()
-    
-    
-# Add a button to load the fruit
-if streamlit.button('Get Fruit List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
-
-
-#Allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values (' " + add_my_fruit + " ')")
-         return "Thanks for adding " + new_fruit
-        
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a Fruit to List'):
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-   back_from_function = insert_row_snowflake(add_my_fruit)
-   streamlit.text(back_from_function)
+-#Allow the end user to add a fruit to the list
+#def insert_row_snowflake(new_fruit):
+#    with my_cnx.cursor() as my_cur:
+#         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values (' " + add_my_fruit + " ')")
+#         return "Thanks for adding " + new_fruit
+#        
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#if streamlit.button('Add a Fruit to List'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   back_from_function = insert_row_snowflake(add_my_fruit)
+#   streamlit.text(back_from_function)
 
 
 
